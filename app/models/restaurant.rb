541ca718764f8f04e65b07a6e5269a4604307712
Marnie Nyenhuis <marnie.nyenhuis@t-online.de>
2021-05-19 15:18:52 +0200

    Restaurant and review nested structure and view completed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: destroy
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+
+  def ratings_average
+    reviews.present? ? reviews.map(&:rating).sum / reviews.length : 0
+  end
 end
